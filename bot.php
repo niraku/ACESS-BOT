55e79c2018e9b25c604eae41ff4d8ff6f891a858
Dev <charoenrat.tu@gmail.com>
2017-05-25 15:30:05 +0700

    verify code

--- bot.php
+++ bot.php
@@ -1,5 +1,4 @@
 <?php
-<?php
 $access_token = '+8h4N62YdV0CDHhG+hDMBFK/5G6MuyVjKqxawx1nWQM2QKy1jiEcGte+5LgI/Hj1z7rkzEtpmrfLVbQV6cOpj9HGVWYW5YbKGoxp59FtOkFBHmuRjKygcWzqJDoLtcgBB9n+cQG6WJ0ur23yNPI9bAdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
